718569ec78889e40fed727f94f3565581c938efe
Piotr Pupczyk <ppsirius@gmail.com>
2015-11-14 19:37:48 +0100

    - update mixins

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    var mediaQuery = $('.viewports').css('font-family');
+
+    // Media Query for js
+    //if (mediaQuery == 'tablet' || mediaQuery == 'desktop') {
+    //
+    //}
+
+});
